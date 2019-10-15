dd095652afbbc2349d9092c4f013cd6d013b02d2
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-15 14:53:53 +0300

    Update N3.cpp

--- N3.cpp
+++ N3.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cmath>
 
@@ -5,10 +6,12 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int ax, ay, bx, by, cx, cy;
+	int ax, ay, bx, by, cx, cy, AB, AC;
 	cin >> ax >> ay >> bx >> by >> cx >> cy;
-	if (sqrt((ax - bx)*(ax - bx) + (ay - by)*(ay - by)) > sqrt((ax - cx)*(ax - cx) + (ay - cy)*(ay - cy))) cout << "Точка С ближе\n";
-	else if (sqrt((ax - bx)*(ax - bx) + (ay - by)*(ay - by)) < sqrt((ax - cx)*(ax - cx) + (ay - cy)*(ay - cy))) cout << "Точка B ближе\n";
+	AB = sqrt((ax - bx)*(ax - bx) + (ay - by)*(ay - by));
+	AC = sqrt((ax - cx)*(ax - cx) + (ay - cy)*(ay - cy));
+	if (AB > AC) cout << "Точка С ближе\n";
+	else if (AB < AC) cout << "Точка B ближе\n";
 	else cout << "Одинаковое расстояние\n";
 	system("pause");
 	return 0;
