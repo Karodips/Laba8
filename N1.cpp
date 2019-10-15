e1dac4f063071bd554449feb44169cef9ff593ea
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-15 14:43:40 +0300

    Update N1.cpp

--- N1.cpp
+++ N1.cpp
@@ -9,17 +9,15 @@ int main() {
 	cin >> a >> b;
 	if (a > b) {
 		b = a;
-		cout << a << " " << b << endl;
 	}
 	else if (b > a) {
 		a = b;
-		cout << a << " " << b << endl;
 	}
 	else {
 		a = 0;
 		b = 0;
-		cout << a << " " << b << endl;
 	}
+	cout << a << " " << b << endl;
 	system("pause");
 	return 0;
 }
